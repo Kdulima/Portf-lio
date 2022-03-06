bb57f7f607a43437793b46d2e63229672224fd20
carloslima <carlos.eduardo@the9level.com>
2022-03-06 20:18:44 -0300

    feat: added header section

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,7 +1,19 @@
 import logo from '../assets/logo.png';
 import logo1 from '../assets/logo_1.png';
+import profile from '../assets/profile.svg';
+import circle from '../assets/circle.svg';
+import bgIMG from '../assets/bgIMG.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
 
 export default {
   logo,
   logo1,
+  profile,
+  circle,
+  bgIMG,
+  react,
+  redux,
+  sass,
 };
